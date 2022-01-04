c8e4d7997124c006e79c8370f6d2da2a670761e6
mwllf <mwllf@Valerian>
2022-01-04 17:57:10 +0700

    Tugas_Money_Converter_KelasB_20200140051

---
+++ src/main/java/tugas/java/b/tugas/java/TugasRumahJava.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.java.b.tugas.java;
+
+/**
+ *
+ * @author mwllf
+ */
+public class TugasRumahJava {
+    
+    public String GBPoundtoIDRupiah (){
+      double GBPound = 1;
+      double IDRupiah = 19.289;
+      return " Conversion Result " + GBPound + " GBP to IDR is " + IDRupiah;
+    }
+    
+    public String USDollartoGBPound (){
+      double USDollar  = 1;
+      double GBPound = 1.356;
+      return " Conversion Result " + USDollar + " USD to GBP is " + GBPound;
+    }
+    
+    public String RupiahtoBaht (){ 
+      double IDRupiah = 10.000;
+      double THBaht = 22.95;
+      return " Conversion Result " + IDRupiah + " IDR to THB is " + THBaht;
+    }
+    
+    
+}
---
+++ src/test/java/tugas/java/b/tugas/java/ApplicationTests.java
@@ -0,0 +1,13 @@
+package tugas.java.b.tugas.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
